ad0e8b65b58194720b56149847f2233dbca919d9
Safa G <59778894+safag05@users.noreply.github.com>
2020-07-07 17:33:59 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "google_domain_name" {}
 variable "kibana_ip" {
     type = "map"
     default = { 
-        kibana_ip_ranges = "24.15.232.38/32, 50.194.68.229/32, 10.16.0.27/8"
+        kibana_ip_ranges = "24.15.232.38/32, 50.194.68.229/32, 10.16.0.27/8, 50.194.68.230/32"
     }
 }
 
