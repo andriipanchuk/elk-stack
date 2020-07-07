e8ac07acc3894eebcd1077c6ebb8e1faa1c12f7b
Farkhod <sadykovfarkhod@gmail.com>
2020-07-07 18:50:11 -0500

    Created all file and configured to proper folders

--- variables.tf
+++ variables.tf
@@ -1,11 +1,17 @@
-variable "deployment_endpoint" {}
 
-variable "google_domain_name" {}
+variable "google_domain_name" {
+}
 
-variable "kibana_ip" {
+variable "elk" {
     type = "map"
     default = { 
-        kibana_ip_ranges = "24.15.232.38/32, 50.194.68.229/32, 10.16.0.27/8, 50.194.68.230/32"
+        whitelisted_ip_ranges = "24.15.232.38/32, 50.194.68.229/32, 10.16.0.27/8, 50.194.68.230/32"
     }
 }
 
+
+variable "deployment_name" {
+}
+
+variable "deployment_environment" {
+}
